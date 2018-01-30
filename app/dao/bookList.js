ee4e2dc56df1673cc40d4021b03703853fe71378
antonstrand <jag@antonstrand.se>
2018-01-30 20:30:31 +0100

    Subtask A

---
+++ app/dao/bookList.js
@@ -0,0 +1,38 @@
+'use strict'
+
+module.exports = function () {
+  return [
+    { 
+      author: 'Isaac Asimov',
+      title: 'Foundation',
+      genre: 'Science Ficition',
+      price: 164,
+      publish_date: '1951-08-21',
+      description: 'Foundation is the first novel in Isaac Asimovs Foundation Trilogy (later expanded into The Foundation Series). Foundation is a cycle of five interrelated short stories, first published as a single book by Gnome Press in 1951. Collectively they tell the story of the Foundation, an institute to preserve the best of galactic civilization after the collapse of the Galactic Empire'
+    },
+    {
+      author: 'Isaac Asimov',
+      title: 'Foundation and Empire',
+      genre: 'Science Ficition',
+      price: 79,
+      publish_date: '1952-10-12',
+      description: 'Foundation and Empire is a novel written by Isaac Asimov that was published by Gnome Press in 1952. It is the second book published in the Foundation Series, and the fourth in the in-universe chronology. It takes place in two halves, originally published as separate novellas. The second part, The Mule, won a Hugo Award.'
+    },
+    {
+      author: 'Isaac Asimov',
+      title: 'Second Foundation',
+      genre: 'Science Fiction',
+      price: 79,
+      publish_date: '1953-05-10',
+      description: 'Second Foundation consists of two previously published novellas originally published in appearing in Astounding Magazine (with different titles) between 1948 and 1950, making this the third volume in Asimovs Foundation series. Decades later, Asimov wrote two further sequel novels and two prequels. Later writers have added authorized tales to the series. The Foundation series is often regarded as one of Isaac Asimovs best works, along with his Robot series.'
+    },
+    {
+      author: 'Gamma, Erich; Helm, Richard; Johnson, Ralph; Vlissides, John',
+      title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
+      genre: 'Computer Science',
+      price: 350,
+      publish_date: '1994-10-21',
+      description: 'Design Patterns: Elements of Reusable Object-Oriented Software is a software engineering book describing recurring solutions to common problems in software design. The books authors are Erich Gamma, Richard Helm, Ralph Johnson and John Vlissides with a foreword by Grady Booch. The book is divided into two parts, with the first two chapters exploring the capabilities and pitfalls of object-oriented programming, and the remaining chapters describing 23 classic software design patterns. The book includes examples in C++ and Smalltalk.'
+    }  
+  ]
+}
