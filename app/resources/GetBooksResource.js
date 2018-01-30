dc01a9c16f8730027f3947ab08fcf9460b19873e
antonstrand <jag@antonstrand.se>
2018-01-30 20:48:47 +0100

    Adding name to the function

--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -5,7 +5,7 @@
     var bookList = require('../dao/bookList');
 
 
-    module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
+    module.exports = function getBooks (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
         console.log(bookList())
     };
 
