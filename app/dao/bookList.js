ae3fdd5e5a3f182871595426bce180873551cae7
antonstrand <jag@antonstrand.se>
2018-01-31 12:47:30 +0100

    Turning the book array into JSON

--- app/dao/bookList.js
+++ app/dao/bookList.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = function () {
-  return [
+  return JSON.stringify([
     { 
       author: 'Isaac Asimov',
       title: 'Foundation',
@@ -34,5 +34,5 @@ module.exports = function () {
       publish_date: '1994-10-21',
       description: 'Design Patterns: Elements of Reusable Object-Oriented Software is a software engineering book describing recurring solutions to common problems in software design. The books authors are Erich Gamma, Richard Helm, Ralph Johnson and John Vlissides with a foreword by Grady Booch. The book is divided into two parts, with the first two chapters exploring the capabilities and pitfalls of object-oriented programming, and the remaining chapters describing 23 classic software design patterns. The book includes examples in C++ and Smalltalk.'
     }  
-  ];
+  ]);
 };
