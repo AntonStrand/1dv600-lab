a094c00961b12cdc041267bac6efdcd3bb976dba
antonstrand <jag@antonstrand.se>
2018-01-31 12:23:27 +0100

    Adding semicolons to follow standard.

--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -6,7 +6,7 @@
 
 
     module.exports = function getBooks (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
-        console.log(bookList())
+        console.log(bookList());
     };
 
 }());
