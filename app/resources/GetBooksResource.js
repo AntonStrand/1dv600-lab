d54323a0e6c0c8bde4d8d9b50fa9bbd5e6f4c126
antonstrand <jag@antonstrand.se>
2018-02-02 12:49:08 +0100

    Fixing book object keys

--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -6,7 +6,8 @@
 
 
     module.exports = function getBooks (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
-        console.log(bookList());
+        // console.log(bookList());
+        callback(bookList());
     };
 
 }());
